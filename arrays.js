ea5c631963b8cde4695632cb26655175067be76e
Azumi Callanan <azucm@hotmail.com>
2018-01-07 20:34:41 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,9 @@
 function addElementToBeginningOfArray(array, element){
-  
+  var newArray = [element,...array];
+  return newArray;
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
-  
+  array.unshift(element);
+  return array;
 }
